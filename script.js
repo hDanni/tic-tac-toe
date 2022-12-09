eda192e9ea725b954f5a646121f4f741e228f7c6
csDan31 <csDan31@gmail.com>
2022-12-09 13:54:08 -0500

    use IFFE to control gameBoard and flow of game

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-let gameBoard = {
-    board : [],
-};
-
-let displayController = {
-    render: () => {
-        
-    }
-};
-
 const player = () => {
 
 };
 
-let gameFlow = {
+(function() {
+    let gameBoard = {
+        board : ['0','1','2','3','4','5','6','7','8'],
+        init: function () {
+            this.cacheDom();
+            this.render();
+            
+        },
+        cacheDom: function (){
+            this.start = document.getElementsByClassName('.start');
+            this.restart = document.getElementsByClassName('.restart');
+            this.gameBoard = document.getElementById('gameBoard');
+            this.gridItem = document.querySelectorAll('.grid-item');
+        },
+        render: function (){
+            let data = {
+                board: this.board
+            };
+        },
+        displayController: function (){
+            
+        }
+    };
 
-};
+    gameBoard.init();
+
+})()
