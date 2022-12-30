acceec4ca6e12c55a0d29db5bbf8432d0fc026ac
csDan31 <csDan31@gmail.com>
2022-12-30 01:36:14 -0500

    created render function to display board array

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tic-tac-toe
+
 The Odin Project's Tic Tac Toe Game made with HTML, CSS & JS with emphasis on Modular JS & factory functions.
--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-const player = () => {
+(function () {
+  let gameBoard = {
+    board: ["0", "1", "2", "3", "4", "5", "6", "7", "X"],
+    init: function () {
+      this.cacheDom();
+      this.render();
+    },
+    cacheDom: function () {
+      this.start = document.getElementsByClassName(".start");
+      this.restart = document.getElementsByClassName(".restart");
+      this.gameBoard = document.getElementsByClassName(".board");
+      this.gridItem = document.querySelectorAll(".grid-item");
+    },
+    render: function () {
+      let data = {
+        board: this.board,
+      };
+      for (i = 0; i < data.board.length; i++) {
+        this.gridItem[i].textContent = data.board[i];
+      }
+    },
+    displayController: function () {},
+  };
 
-};
-
-(function() {
-    let gameBoard = {
-        board : ['0','1','2','3','4','5','6','7','8'],
-        init: function () {
-            this.cacheDom();
-            this.render();
-            
-        },
-        cacheDom: function (){
-            this.start = document.getElementsByClassName('.start');
-            this.restart = document.getElementsByClassName('.restart');
-            this.gameBoard = document.getElementById('gameBoard');
-            this.gridItem = document.querySelectorAll('.grid-item');
-        },
-        render: function (){
-            let data = {
-                board: this.board
-            };
-        },
-        displayController: function (){
-            
-        }
-    };
-
-    gameBoard.init();
-
-})()
+  gameBoard.init();
+})();
