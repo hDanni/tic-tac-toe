935f49834bdf5880027cfbc2c1d4dea670d49de5
csDan31 <csDan31@gmail.com>
2023-01-01 15:46:45 -0500

    add bindEvents function to handle clicks and function to mark box

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const player = () => {};
     board: ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
     init: function () {
       this.cacheDom();
+      this.bindEvents();
       this.render();
     },
+    // handles all targeting of DOM elements //
     cacheDom: function () {
       this.start = document.querySelector(".start");
       this.restart = document.querySelector(".restart");
       this.gameBoard = document.querySelector(".board");
       this.gridItem = document.querySelectorAll(".grid-item");
     },
+    // handles the board display //
     render: function () {
       let data = {
         board: this.board,
@@ -20,6 +23,19 @@ const player = () => {};
       for (i = 0; i < data.board.length; i++) {
         this.gridItem[i].textContent = data.board[i];
       }
+    },
+    // handles DOM events //
+    bindEvents: function () {
+      this.gridItem.forEach((box) => {
+        box.addEventListener("click", this.markBox.bind(this));
+      });
+    },
+
+    markBox: function (e) {
+      // this.gridItem[i].textContent = "X";
+      let mark = e.currentTarget.dataset.index;
+      this.board[mark] = "X";
+      this.render();
     },
     displayController: function () {},
   };
