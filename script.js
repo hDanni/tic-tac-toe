4477d8bed958a5973be44556b2cecdda7ebd60f8
csDan31 <csDan31@gmail.com>
2023-01-06 17:13:58 -0500

    add reset btn function and result display controller

--- script.js
+++ script.js
@@ -3,21 +3,14 @@ const player = () => {};
 (function () {
   let gameBoard = {
     board: ["", "", "", "", "", "", "", "", ""],
-    // board: [
-    //   ["", "", ""],
-    //   ["", "", ""],
-    //   ["", "", ""],
-    // ],
-    // board: [
-    //   ["0", "1", "2"],
-    //   ["3", "4", "5"],
-    //   ["6", "7", "8"],
-    // ],
-    turnCounter: 1,
+    playerTurn: false,
+    gameOver: false,
+    moveCounter: 0,
     init: function () {
       this.cacheDom();
       this.bindEvents();
       this.render();
+      this.checkWin();
     },
     // handles all targeting of DOM elements //
     cacheDom: function () {
@@ -25,17 +18,10 @@ const player = () => {};
       this.restart = document.querySelector(".restart");
       this.gameBoard = document.querySelector(".board");
       this.gridItem = document.querySelectorAll(".grid-item");
+      this.display = document.querySelector("h2");
     },
     // handles the board display //
     render: function () {
-      // let data = {
-      //   board: this.board,
-      // };
-      // for (let i = 0; i < data.board.length; i++) {
-      //   for (let j = 0; j < data.board[i].length; j++) {
-      //     this.gridItem[this.gridCounter].textContent = data.board[i][j];
-      //   }
-      // }
       for (let i = 0; i < this.board.length; i++) {
         this.gridItem[i].textContent = this.board[i];
       }
@@ -45,6 +31,7 @@ const player = () => {};
       this.gridItem.forEach((box) => {
         box.addEventListener("click", this.markBox.bind(this));
       });
+      this.restart.addEventListener("click", this.resetBoard.bind(this));
     },
 
     markBox: function (e) {
@@ -52,25 +39,33 @@ const player = () => {};
 
       if (this.board[mark] !== "") {
         return;
-      } else if (!(this.turnCounter % 2 === 0)) {
+      } else if (this.gameOver === true) {
+        return;
+      } else if (this.playerTurn === false) {
         this.board[mark] = "X";
-      } else if (this.turnCounter % 2 === 0) {
+        this.moveCounter++;
+        this.display.textContent = "Player O turn";
+        this.playerTurn = true;
+      } else if (this.playerTurn === true) {
         this.board[mark] = "O";
+        this.moveCounter++;
+        this.display.textContent = "Player X turn";
+        this.playerTurn = false;
       }
-      this.turnCounter++;
       this.render();
       this.winCondition();
+      this.checkWin();
     },
     winCondition: function () {
       const checkIndices = [
         [0, 1, 2],
         [0, 3, 6],
         [0, 4, 8],
-        [0, 4, 8],
         [1, 4, 7],
         [2, 5, 8],
         [3, 4, 5],
         [6, 7, 8],
+        [6, 4, 2],
       ];
 
       checkIndices.forEach((index) => {
@@ -83,11 +78,29 @@ const player = () => {};
           this.board[a] == this.board[b] &&
           this.board[b] == this.board[c]
         ) {
-          console.log("Player Wins");
-        } else {
-          console.log("Tie Game");
+          this.gameOver = true;
         }
       });
+    },
+    checkWin: function () {
+      if (this.gameOver === true) {
+        if (this.playerTurn) {
+          this.display.textContent = "Player X Wins!";
+        } else {
+          this.display.textContent = "Player O Wins!";
+        }
+      } else if (this.moveCounter === 9) {
+        this.display.textContent = "Tie Game";
+        this.gameOver = true;
+      }
+    },
+    resetBoard: function () {
+      (this.board = ["", "", "", "", "", "", "", "", ""]),
+        (this.playerTurn = false),
+        (this.gameOver = false),
+        (this.moveCounter = 0);
+      this.display.textContent = "";
+      this.render();
     },
   };
 
